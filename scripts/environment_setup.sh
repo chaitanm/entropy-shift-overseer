408439c878ef2c32778051c3b182a2aadb73fbc0
chaitanm <chaitanya.meesala@hotmail.com>
2017-04-15 10:09:20 +0530

    Updating Environment Set up Steps

--- scripts/environment_setup.sh
+++ scripts/environment_setup.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+sudo yum update
+
+sudo yum install ruby
+
+sudo yum install wget
+
+cd /home/ec2-user
+
+wget https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
+
+chmod +x ./install
+
+sudo ./install auto
+
 #Installing tomcat
 sudo mkdir /opt/tomcat/ && cd /opt/tomcat
 
@@ -14,3 +28,7 @@ md5sum apache-tomcat-8.5.11.zip
 sudo unzip apache-tomcat-8.5.11.zip
 
 sudo chmod 700 /opt/tomcat/apache-tomcat-8.5.11/bin/*.sh
+
+sudo yum install java-1.8.0
+
+sudo yum remove java-1.7.0-openjdk
